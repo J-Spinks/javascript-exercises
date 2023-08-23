76b6483b193bc6fb5c1c7d3974107966d32bb6a9
Joseph Spinks <jms2895@outlook.com>
2023-08-23 20:41:30 +0100

    challenge complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, number) {
 
+  if(number < 0 ) return "ERROR";
+  let string = "";
+  let i = 0;
+  while(i < number) {
+    string += word;
+    i++
+  }
+  return string
 };
 
 // Do not edit below this line
