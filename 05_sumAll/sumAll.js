f61b793cf51d38e75e613cf22f4a1e4dee4c72f5
Joseph Spinks <jms2895@outlook.com>
2023-08-30 10:35:49 +0100

    challenge complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (start < 0 || end < 0) {
+    return 'ERROR';
+  }
+
+  if (start !== Number(start) || end !== Number(end)) {
+    return 'ERROR';
+  }
+
+  if (start > end){
+    [start, end] = [end, start];
+  }
+
+  let sum = 0;
+
+  for (let num = start; num <= end; num++) {
+    sum += num;
+
+  }
+
+
+
+  return sum
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// Number(x) !== x || Number(y) !== y
