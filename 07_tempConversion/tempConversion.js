ab4efca27a331ea1f906b16d339899f437b9fb42
Joseph Spinks <jms2895@outlook.com>
2023-08-30 14:43:30 +0100

    challenge complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  x = temp - 32;
+  y = (55.555556 / 100) * x;
+
+  toDecimalPlace = y.toFixed(1);
+
+  result = parseFloat(toDecimalPlace);
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  x = (180 / 100) * temp
+
+  y = x + 32
+
+
+   toDecimalPlace = y.toFixed(1);
+
+   result = parseFloat(toDecimalPlace);
+   return result
+
+
 };
 
 // Do not edit below this line
